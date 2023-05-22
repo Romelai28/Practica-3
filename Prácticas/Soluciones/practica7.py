42379d80f858a342188d5216353b90b9784aad82
Mauricio Romero Laino <Romelai28@gmail.com>
2023-05-22 04:37:00 -0300

    hasta ej 3

--- Prácticas/Soluciones/practica7.py
+++ Prácticas/Soluciones/practica7.py
@@ -59,4 +59,5 @@ def problema_es_nombre_largo(nombre: str) -> bool:
 def es_bisiesto(year: int) -> bool:
     return ((year % 400 == 0) or (year % 4 == 0 and year % 100 != 0))
 
-print(es_bisiesto(2500))
+# EJERCICIO 4:
+
