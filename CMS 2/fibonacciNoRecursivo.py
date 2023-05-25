8c87e41b5b8018bf49153ce78a607ad7b714b9e4
Mauricio Romero Laino <Romelai28@gmail.com>
2023-05-25 20:48:16 -0300

    ej 2 terminado

--- CMS 2/fibonacciNoRecursivo.py
+++ CMS 2/fibonacciNoRecursivo.py
@@ -1,8 +1,19 @@
 import sys
 
+
 def fibonacciNoRecursivo(n: int) -> int:
-  # Implementar esta funcion
-  return 0
+    # Implementar esta funcion
+    # Requiere: n >= 0
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        res = [0, 1]
+        for i in range(2, n+1):
+            res.append(res[i-2] + res[i-1])
+        # return res  # Visualizar la succesión.
+        return res[n]
 
 
 if __name__ == '__main__':
