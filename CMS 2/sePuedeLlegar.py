1f5586912afeb003c807c23e6945d8bf38545a00
Mauricio Romero Laino <Romelai28@gmail.com>
2023-05-26 03:56:21 -0300

    ej 5 terminado

--- CMS 2/sePuedeLlegar.py
+++ CMS 2/sePuedeLlegar.py
@@ -5,9 +5,23 @@ from typing import Tuple
 # l: List[int]  <--Este es un ejemplo para una lista de enteros.
 # t: Tuple[str,str]  <--Este es un ejemplo para una tupla de strings.
 # Respetar esta sintaxis, ya que el CMS dirá que no pasó ningún test si usan otra notación.
-def sePuedeLlegar(origen: str, destino: str, vuelos: List[Tuple[str, str]]) -> int :
-  # definir esta función
-  return 0
+
+
+def sePuedeLlegar(origen: str, destino: str, vuelos: List[Tuple[str, str]]) -> int:
+    # definir esta función
+    ciudad_actual = origen
+    contador_de_viajes: int = 0
+    while ciudad_actual != destino:
+        contador_de_viajes_antiguo = contador_de_viajes
+        for i in range(len(vuelos)):
+            if ciudad_actual == vuelos[i][0]:
+                ciudad_actual = vuelos[i][1]  # Hacer el viaje
+                contador_de_viajes += 1
+                break
+        if contador_de_viajes == contador_de_viajes_antiguo:  # Significa que no pudo hacer ningún viaje, fin del camino
+            return -1
+    return contador_de_viajes
+
 
 if __name__ == '__main__':
   origen = input()
