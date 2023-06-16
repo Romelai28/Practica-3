49e299840bba72530a8f7c02a591f589eb1ef7a3
Mauricio Romero Laino <Romelai28@gmail.com>
2023-06-16 00:50:04 -0300

    Ej 1 terminado

--- CMS 3/polaco_inverso.py
+++ CMS 3/polaco_inverso.py
@@ -4,8 +4,31 @@ from queue import LifoQueue
 # para el tipo pila de enteros, usar: "pila: LifoQueue". La notación "pila: LifoQueue[float]" no funciona.
 def calcular_expresion(expr: str) -> float:
     # implementar función con el TAD Pila
-    return 0
-    
+    pila = LifoQueue()
+
+    operadores = ["*", "+", "/", "-"]
+
+    expresion_split = expr.split()
+    for i in expresion_split:
+       
+       if i not in operadores:
+        pila.put(i)
+
+       else:
+          a = pila.get()
+          b = pila.get()
+
+          if i == "*":
+             pila.put(float(b) * float(a))
+          elif i == "+":
+             pila.put(float(b) + float(a))
+          elif i == "/":
+             pila.put(float(b) / float(a))
+          else:
+             pila.put(float(b) - float(a))
+
+    return pila.get()
+
     
 if __name__ == '__main__':
   x = input() # Por ejemplo: 2 5 * 7 +
