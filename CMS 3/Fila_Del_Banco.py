d2b1bf5cce1e267b2aa8d256851896fe88e10f72
Mauricio Romero Laino <Romelai28@gmail.com>
2023-06-19 00:56:14 -0300

    Ej 4 terminado

--- CMS 3/Fila_Del_Banco.py
+++ CMS 3/Fila_Del_Banco.py
@@ -2,7 +2,67 @@ from queue import Queue
 
 # El tipo de fila debería ser Queue[int], pero la versión de python del CMS no lo soporta. Usaremos en su lugar simplemente "Queue"
 def avanzarFila(fila: Queue, min: int):
-  #implementar función
+    #implementar función
+    n: int = longitud_cola(fila)
+    minuto_de_entrada: int = -10 # Valor absurdo de inicialización
+
+    for minuto in range(min + 1):
+        
+        # Caja 1:
+        if not fila.empty():
+            if minuto % 10 == 1:
+                fila.get()
+
+        # Caja 2:
+        if not fila.empty():
+            if minuto % 4 == 3:
+                fila.get()
+
+        # Caja 3:
+        if not fila.empty():
+            if minuto % 4 == 2:
+                minuto_de_entrada = minuto
+                persona_a_volver: int = fila.get()
+          
+        # Regreso de la persona de caja 3:
+        if minuto == minuto_de_entrada + 3:
+            fila.put(persona_a_volver)
+    
+        # Nueva persona:
+        if minuto % 4 == 0:
+            n += 1
+            fila.put(n)
+
+    return fila
+
+
+def cola_a_lista(cola):
+    lista_elem: list = []
+    while not cola.empty():
+        elem = cola.get()
+        lista_elem.append(elem)
+    for e in lista_elem:
+        cola.put(e)
+    return lista_elem
+
+
+def lista_a_cola(lista):
+    cola: Queue = Queue()
+    for e in lista:
+        cola.put(e)
+    return cola
+
+
+def longitud_cola(cola: Queue):
+    lista: list = cola_a_lista(cola)
+    res: int = len(lista)
+    cola = lista_a_cola(lista)
+    return res
+
+
+# Testing
+#print(cola_a_lista(avanzarFila(lista_a_cola([1,2,3]), 100)))
+
 
 if __name__ == '__main__':
   fila: Queue = Queue()
